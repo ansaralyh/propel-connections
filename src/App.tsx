33750b6b0ca498e209c4f4a04bcd11db921b41bb
Zultaif Hassan <zultaifhassan110@gmail.com>
2025-07-31 05:08:39 +0500

    routing added

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
-import './App.css'
-import Footer from './components/footer/Footer'
-import Navbar from './components/navbar/Navbar'
-
-
+import "./App.css";
+import Footer from "./components/footer/Footer";
+import Navbar from "./components/navbar/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Careers from "./pages/careers/Careers";
+import Affliates from "./pages/affliates/Affliates";
+import Press from "./pages/press/Press";
 
 function App() {
-
   return (
     <>
-     <Navbar />
-     <Footer />
+      <Navbar />
+      <Routes>
+        <Route path="/careers" element={<Careers />} />
+        <Route path="/affliates" element={<Affliates />} />
+        <Route path="/press" element={<Press />} />
+      </Routes>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
